3b841591eee7c360de409da3605692c72031d25c
Radoslav Genov <radoslavgenov5@hotmail.com>
2017-07-18 22:22:57 +0300

    cleaned up some shit

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -3,37 +3,69 @@
  */
 
 function parallax() {
-    var controller = new ScrollMagic.Controller({vertical: false});
-    var $bulb = $(".st0"),
-        $word = $(".st1");
+    var controller = new ScrollMagic.Controller({vertical: false}),
+        tweenBG = new TimelineMax(),
+        tweenSectionTwo = new TimelineMax(),
+        tweenSectionFour = new TimelineMax(),
+        tweenBulb = new TimelineMax(),
+        tweenWord = new TimelineMax(),
+        sceneBG = new ScrollMagic.Scene({
+            triggerElement: '#bg',
+            duration: $('#bg').width()
+        }),
+        sceneSectionTwo = new ScrollMagic.Scene({
+            triggerElement: "#one",
+            triggerHook: 0,
+            duration: $(window).width()
+        }),
+        sceneSectionFour = new ScrollMagic.Scene({
+            triggerElement: "#three",
+            triggerHook: 0,
+            duration: $(window).width()
+        }),
+        sceneBulb = new ScrollMagic.Scene({
+            triggerElement: "#three",
+            triggerHook: 0.5,
+            duration: 700,
+            tweenChanges: true
+        }),
+        sceneWord = new ScrollMagic.Scene({
+            triggerElement: "#three",
+            triggerHook: 0.5,
+            duration: 700,
+            tweenChanges: true
+        }),
+        $bulb = $(".st0"),
+        $word = $(".st1"),
+        bulbCollection = [],
+        wordCollection = [];
 
-    var tweenBG = new TimelineMax()
-        .add([
-            TweenMax.fromTo($('#bg-dots-one'), 1, {backgroundPosition: '0% 0%'}, {
-                backgroundPosition: '-20% 0%',
-                ease: Linear.easeNone
-            })
-        ]);
+    //parallax
+    tweenBG.add([
+        TweenMax.fromTo($('#bg-dots-one'), 1, {backgroundPosition: '0% 0%'}, {
+            backgroundPosition: '-20% 0%',
+            ease: Linear.easeNone
+        })
+    ]);
 
-    var tweenSectionTwo = new TimelineMax()
-        .add([
-            TweenMax.fromTo('#container-section-two', 1, {autoAlpha: 0, x: 500}, {
-                autoAlpha: 1,
-                x: 0,
-                ease: Linear.easeNone
-            })
-        ]);
+    //section tween
+    tweenSectionTwo.add([
+        TweenMax.fromTo('#container-section-two', 1, {autoAlpha: 0, x: 500}, {
+            autoAlpha: 1,
+            x: 0,
+            ease: Linear.easeNone
+        })
+    ]);
 
-    var tweenSectionFour = new TimelineMax()
-        .add([
-            TweenMax.fromTo('#container-section-four', 1, {autoAlpha: 0, x: 500}, {
-                autoAlpha: 1,
-                x: 0,
-                ease: Linear.easeNone
-            })
-        ]);
+    tweenSectionFour.add([
+        TweenMax.fromTo('#container-section-four', 1, {autoAlpha: 0, x: 500}, {
+            autoAlpha: 1,
+            x: 0,
+            ease: Linear.easeNone
+        })
+    ]);
 
-
+    //svg drawing
     function pathPrepare($el) {
         var lineLength = $el[0].getTotalLength();
         $el.css("stroke-dasharray", lineLength);
@@ -44,180 +76,40 @@ function parallax() {
     $.each($bulb, function (i, val) {
         pathPrepare($(val));
     });
-    // prepare SVG
+
     $.each($word, function (i, val) {
         pathPrepare($(val));
     });
 
-    // build tween
-    var tweenBulb = new TimelineMax()
-        .add([
+    for (let i = 0; i < $bulb.length; i++) {
+        bulbCollection[i] = TweenMax.fromTo($bulb[i], 1.4, {
+            autoAlpha: 0,
+            strokeDashoffset: $bulb[i].getTotalLength(),
+            ease: Linear.easeNone
+        }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone})
+    }
 
-            TweenMax.fromTo($bulb[0], 1.4, {
-                autoAlpha: 0,
-                strokeDashoffset: $bulb[0].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}),
-            TweenMax.fromTo($bulb[1], 1.4, {
-                autoAlpha: 0,
-                strokeDashoffset: $bulb[1].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}),
-            TweenMax.fromTo($bulb[2], 1.4, {
-                autoAlpha: 0,
-                strokeDashoffset: $bulb[2].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}),
-            TweenMax.fromTo($bulb[3], 1.4, {
-                autoAlpha: 0,
-                strokeDashoffset: $bulb[3].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}),
-            TweenMax.fromTo($bulb[4], 1.4, {
-                autoAlpha: 0,
-                strokeDashoffset: $bulb[4].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}),
-            TweenMax.fromTo($bulb[5], 1.4, {
-                autoAlpha: 0,
-                strokeDashoffset: $bulb[5].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}),
-            TweenMax.fromTo($bulb[6], 1.4, {
-                autoAlpha: 0,
-                strokeDashoffset: $bulb[6].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}),
-            TweenMax.fromTo($bulb[7], 1.4, {
-                autoAlpha: 0,
-                strokeDashoffset: $bulb[7].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}),
-            TweenMax.fromTo($bulb[8], 1.4, {
-                autoAlpha: 0,
-                strokeDashoffset: $bulb[8].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}),
-            TweenMax.fromTo($bulb[9], 1.4, {
-                autoAlpha: 0,
-                strokeDashoffset: $bulb[9].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}),
-            TweenMax.fromTo($bulb[10], 1.4, {
-                autoAlpha: 0,
-                strokeDashoffset: $bulb[10].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}),
-            TweenMax.fromTo($bulb[11], 1.4, {
-                autoAlpha: 0,
-                strokeDashoffset: $bulb[11].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone})
-        ]);// change color during the whole thing
 
-    // build tween
-    var tweenWord = new TimelineMax()
-        .add(
-            TweenMax.fromTo($word[0], 0.2, {
-                autoAlpha: 0,
-                strokeDashoffset: $word[0].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}))
-        .add(
-            TweenMax.fromTo($word[1], 0.2, {
-                autoAlpha: 0,
-                strokeDashoffset: $word[1].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}))
-        .add(
-            TweenMax.fromTo($word[2], 0.2, {
-                autoAlpha: 0,
-                strokeDashoffset: $word[2].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}))
-        .add(
-            TweenMax.fromTo($word[3], 0.2, {
-                autoAlpha: 0,
-                strokeDashoffset: $word[3].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}))
-        .add(
-            TweenMax.fromTo($word[4], 0.2, {
-                autoAlpha: 0,
-                strokeDashoffset: $word[4].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}))
-        .add(
-            TweenMax.fromTo($word[5], 0.2, {
-                autoAlpha: 0,
-                strokeDashoffset: $word[5].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}))
-        .add(
-            TweenMax.fromTo($word[6], 0.2, {
-                autoAlpha: 0,
-                strokeDashoffset: $word[6].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}))
-        .add(
-            TweenMax.fromTo($word[7], 0.3, {
-                autoAlpha: 0,
-                strokeDashoffset: $word[7].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}))
-        .add(
-            TweenMax.fromTo($word[8], 0.3, {
-                autoAlpha: 0,
-                strokeDashoffset: $word[8].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}))
-        .add(
-            TweenMax.fromTo($word[9], 0.3, {
-                autoAlpha: 0,
-                strokeDashoffset: $word[9].getTotalLength(),
-                ease: Linear.easeNone
-            }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone}));// change color during the whole thing
+    for (let i = 0; i < $word.length; i++) {
+        wordCollection[i] = TweenMax.fromTo($word[i], 0.2, {
+            autoAlpha: 0,
+            strokeDashoffset: $word[i].getTotalLength(),
+            ease: Linear.easeNone
+        }, {autoAlpha: 1, strokeDashoffset: 0, ease: Linear.easeNone})
+    }
 
-    var sceneBG = new ScrollMagic.Scene({triggerElement: '#bg', duration: $('#bg').width()})
-        .setTween(tweenBG)
-        //.addIndicators() // add indicators (requires plugin) **** shows where trigger begins and end
-        .addTo(controller);
+    tweenBulb.add(bulbCollection);
+    tweenWord.add(wordCollection);
 
-    var sceneSectionTwo = new ScrollMagic.Scene({triggerElement: "#one", triggerHook: 0, duration: $(window).width()})
-        .setTween(tweenSectionTwo)
-        //.addIndicators() // add indicators (requires plugin) **** shows where trigger begins and end
-        .addTo(controller);
+    sceneBG.setTween(tweenBG).addTo(controller);
 
-    var sceneSectionFour = new ScrollMagic.Scene({
-        triggerElement: "#three",
-        triggerHook: 0,
-        duration: $(window).width()
-    })
-        .setTween(tweenSectionFour)
-        //.addIndicators() // add indicators (requires plugin) **** shows where trigger begins and end
-        .addTo(controller);
+    sceneSectionTwo.setTween(tweenSectionTwo).addTo(controller);
 
-    // build scene
-    var sceneBulb = new ScrollMagic.Scene({
-        triggerElement: "#three",
-        triggerHook: 0.5,
-        duration: 700,
-        tweenChanges: true
-    })
-        .setTween(tweenBulb)
-        //.addIndicators() // add indicators (requires plugin)
-        .addTo(controller);
+    sceneSectionFour.setTween(tweenSectionFour).addTo(controller);
 
-    // build scene
-    var sceneWord = new ScrollMagic.Scene({
-        triggerElement: "#three",
-        triggerHook: 0.5,
-        duration: 700,
-        tweenChanges: true
-    })
-        .setTween(tweenWord)
-        //.addIndicators() // add indicators (requires plugin)
-        .addTo(controller);
+    sceneBulb.setTween(tweenBulb).addTo(controller);
+
+    sceneWord.setTween(tweenWord).addTo(controller);
 }
 
 parallax();
